33b1fb911c1f28ff498fba511d1515d9628fd66c
qinxiongzhou <363606635@qq.com>
2019-08-23 20:45:07 +0800

    update project

--- limiting-design/counter/src/main/java/com/ryan/counter/controller/Controller.java
+++ limiting-design/counter/src/main/java/com/ryan/counter/controller/Controller.java
@@ -1,6 +1,9 @@
 package com.ryan.counter.controller;
 
 import com.ryan.counter.limit.LimitCounter;
+import com.ryan.counter.limit.LimitCounterInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 */
 @RestController
 public class Controller {
+    private static Logger logger = LoggerFactory.getLogger(LimitCounterInterceptor.class);
+
     @GetMapping("limit")
     @LimitCounter(concurrentNum="0")
     public String limit(){
         System.out.println("limit interface");
         return "limit interface";
     }
+
 }
